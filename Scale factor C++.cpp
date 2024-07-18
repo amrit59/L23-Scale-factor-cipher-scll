6dbce9c5298524fa46ae50c460e100875bf9fcc5
Amrit Raj <121791502+amrit59@users.noreply.github.com>
2024-07-18 16:11:05 +0530

    Add files via upload

---
+++ Scale factor C++.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a=5;
+	int *x;
+	x=&a;
+cout<<"the address of a , which is stored in the pointer x is: "<<x<<" The value of x+1 is "<<x+1<<endl;
+	return 0;
+}
