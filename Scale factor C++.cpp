0cd3618f0d0ac41e27e425f30bafc0bebb773bef
Amrit Raj <121791502+amrit59@users.noreply.github.com>
2024-07-18 16:21:33 +0530

    Update Scale factor C++.cpp

--- Scale factor C++.cpp
+++ Scale factor C++.cpp
@@ -7,3 +7,19 @@ int main(){
 cout<<"the address of a , which is stored in the pointer x is: "<<x<<" The value of x+1 is "<<x+1<<endl;
 	return 0;
 }
+
+
+2nd code 
+
+#include<iostream>
+using namespace std;
+int main(){
+	int a[5];
+	int *x;
+	x=&a[0];
+	int i;
+	for(i=0;i<6;i++){
+		cout<<a[i]<<endl;	
+	} 
+	return 0;
+}
